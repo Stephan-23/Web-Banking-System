876047ecf02af3fca8755729f5d7f27e4d6f9788
Thembinkosi Dladla <thembinkosidladla318@gmail.com>
2025-04-02 15:01:23 +0200

    Initial commit: Set up Spring Boot project with MySQL

---
+++ Web-Banking-System/src/main/java/com/webApp/Web_Banking_System/WebBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.webApp.Web_Banking_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBankingSystemApplication.class, args);
+	}
+
+}
---
+++ Web-Banking-System/src/test/java/com/webApp/Web_Banking_System/WebBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webApp.Web_Banking_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
